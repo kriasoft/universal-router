6745a0f2983f8a395bf0c7dfaff672cb35b73617
Pavel Lang <langpavel@phpskelet.org>
2019-04-09 20:01:38 +0200

    test(coverage): set low limits

--- jest.config.js
+++ jest.config.js
@@ -13,10 +13,10 @@ const config = {
   testPathIgnorePatterns: ['dist'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
     },
   },
   setupFiles: ['<rootDir>/config/setup-tests.js'],
